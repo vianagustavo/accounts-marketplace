e4920e0c0785f4489b76d0719c14bbfbcff8b2b2
Gustavo <gustavo.ferreira@kinvo.com.br>
2024-02-04 18:15:33 -0300

    feat: add cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AccountsDiscordModule } from './modules/discord/discord.module';
 import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './health.controller';
+import { CronService } from './health.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     AccountsDiscordModule,
   ],
   controllers: [HealthController],
-  providers: [],
+  providers: [CronService],
 })
 export class AppModule {}
