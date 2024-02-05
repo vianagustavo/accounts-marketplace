4155daa7cb48c4793f99a27827aea54cdecdadca
Gustavo <gustavo.ferreira@kinvo.com.br>
2024-02-05 10:25:45 -0300

    feat: add cron service

---
+++ src/cron.service.ts
@@ -0,0 +1,20 @@
+// cron.service.ts
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
+
+@Injectable()
+export class CronService {
+  @Cron(CronExpression.EVERY_MINUTE)
+  async performHealthCheck(): Promise<void> {
+    try {
+      const response = await axios.get(
+        'https://accounts-marketplace.onrender.com/health',
+      );
+
+      console.log('Health Check Response:', response.data);
+    } catch (error) {
+      console.error('Health Check Error:', error.message);
+    }
+  }
+}
