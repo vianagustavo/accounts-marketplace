729757ff42c2fa4803d33988c542dec9386ad50c
Gustavo <gustavo.ferreira@kinvo.com.br>
2024-02-04 22:29:33 -0300

    chore: add health controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AccountsDiscordModule } from './modules/discord/discord.module';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     ScheduleModule.forRoot(),
     AccountsDiscordModule,
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
